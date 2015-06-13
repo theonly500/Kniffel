bc9e5ff3882abee8edbb26f4fe58a728a0556bf2
theonly500 <rescue.theonly@gmail.com>
2015-06-13 22:51:25 +0200

    Version 0.0.1

--- src/UI.java
+++ src/UI.java
@@ -27,11 +27,19 @@ public class UI {
         //create JFrame to contain every element
         frame=new JFrame("Yahtzee");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
+        //add the base panel to the frame
         base=new JPanel();
         base.setLayout(new BoxLayout(base, BoxLayout.X_AXIS));
         frame.add(base);
+        //add the panel for all dices
         diceBase=new JPanel();
+        diceBase.setLayout(new BoxLayout(diceBase, BoxLayout.X_AXIS));
         base.add(diceBase);
+        //create and add the panel for every singular dice
+        individualDiceBase = new  JPanel[5];
+        for (int i=0;i<5;i++){
+            individualDiceBase[i]=new JPanel();
+            individualDiceBase[i].setLayout(new BoxLayout(individualDiceBase[i],BoxLayout.Y_AXIS));
+        }
     }
 }
