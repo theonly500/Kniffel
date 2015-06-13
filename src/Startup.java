678e36523c57b0529b3f238f6a1725b9a0a90a5d
theonly500 <rescue.theonly@gmail.com>
2015-06-13 22:19:45 +0200

    Version 0.0.1

---
+++ src/Startup.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Felix on 11.06.2015.
+ */
+
+public class Startup
+{
+
+    public Startup()
+    {
+    }
+
+    public static void main(String[] args) {
+        FirstFrame firstFrame=new FirstFrame();
+    }
+
+}
