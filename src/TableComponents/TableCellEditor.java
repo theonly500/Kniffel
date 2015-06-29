b38000c0ab09532a558d1533f4bda8982d036108
theonly500 <rescue.theonly@gmail.com>
2015-06-29 17:58:11 +0200

    Version 1.0

---
+++ src/TableComponents/TableCellEditor.java
@@ -0,0 +1,9 @@
+package TableComponents;
+
+import javax.swing.*;
+
+public class TableCellEditor extends DefaultCellEditor {
+    public TableCellEditor(JTextField textField) {
+        super(textField);
+    }
+}
