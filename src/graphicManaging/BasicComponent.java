bdc438db4c1954a71b954b94a3289636be4e1ddd
theonly500 <rescue.theonly@gmail.com>
2015-07-24 13:19:47 +0200

    Beginning to rewrite the Whole programm

---
+++ src/graphicManaging/BasicComponent.java
@@ -0,0 +1,52 @@
+package graphicManaging;
+
+//test
+
+import javax.swing.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class BasicComponent{
+
+    private SpecializedPanel basePanel;
+
+    public BasicComponent() {
+        basePanel = new SpecializedPanel();
+
+    }
+
+    public JPanel getBasePanel() {
+        return basePanel;
+    }
+
+    private void prepareJPanel() {
+        basePanel.addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+
+        });
+    }
+}
+
